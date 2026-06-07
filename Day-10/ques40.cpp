75e6494df178229777b2f3e1d41c78fd8dd0eb21
Madhav Nandan Sharma <nandansharmam@gmail.com>
2026-06-08 00:14:39 +0530

    Fix Day-10 and Day-11 as normal folders

---
+++ Day-10/ques40.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+int i, j;
+ int rows = 5;
+  char ch;
+
+for(i = 1; i <= rows; i++) {
+
+
+ for(j = 1; j <= rows - i; j++) {
+ printf(" ");
+        }
+
+ch = 'A';
+ for(j = 1; j <= i; j++) {
+  printf("%c", ch);
+  ch++;
+ }
+
+
+ ch = ch - 2;
+ for(j = 1; j < i; j++) {
+  printf("%c", ch);
+ ch--;
+ }
+
+ printf("\n");
+}
+
+ return 0;
+}
